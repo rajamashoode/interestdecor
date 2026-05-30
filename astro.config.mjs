f127644aa29ca06ecb2d2959d4a5fa5efa97221b
Raja Mashood Elahi <91584540+rajamashoode@users.noreply.github.com>
2026-05-30 21:30:40 +0500

    Revert: back to simple static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://interestdecor.com',
-  output: 'hybrid',
-  adapter: vercel(),
   trailingSlash: 'always',
 });
