729f2741572877cc4353b982fe6b9e320170cb68
Raja Mashood Elahi <91584540+rajamashoode@users.noreply.github.com>
2026-05-31 20:13:51 +0500

    manualsitemap added & plugin uninstalled

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://interestdecor.com',
   trailingSlash: 'always',
-  integrations: [
-    sitemap({
-      changefreq: 'weekly',
-      priority: 0.7,
-      lastmod: new Date(),
-      customPages: [
-        'https://interestdecor.com/',
-        'https://interestdecor.com/categories/',
-        'https://interestdecor.com/about/',
-        'https://interestdecor.com/contact/',
-      ],
-    }),
-  ],
 });
